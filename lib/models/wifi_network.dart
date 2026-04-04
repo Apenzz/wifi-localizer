7633d7ad719b40df91547795e600d7185ef4ffdc
albertoDev <devalberto98@gmail.com>
2026-04-04 22:10:31 +0200

    add JSON serialization to WifiNetwork and path_provider dependency

--- lib/models/wifi_network.dart
+++ lib/models/wifi_network.dart
@@ -5,4 +5,20 @@ class WifiNetwork {
   final int rssi;
 
   WifiNetwork({required this.bssid, required this.ssid, required this.rssi});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'bssid': bssid,
+      'ssid': ssid,
+      'rssi': rssi,
+    };
+  }
+
+  factory WifiNetwork.fromJson(Map<String, dynamic> json) {
+    return WifiNetwork(
+      bssid: json['bssid'] as String,
+      ssid: json['ssid'] as String,
+      rssi: json['rssi'] as int
+    );
+  }
 }
