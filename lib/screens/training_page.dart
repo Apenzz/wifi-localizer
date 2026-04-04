c132127d4ae31cf3ad2ae2206109d6331099cb1b
albertoDev <devalberto98@gmail.com>
2026-04-04 10:49:10 +0200

    add label input and collect sample button to TrainingPage

--- lib/screens/training_page.dart
+++ lib/screens/training_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class TrainingPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Placeholder(), // TODO
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: 250,
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Label',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () => print('pressed'),
+            child: Text('Collect Sample'),
+          ),
+        ],
+      ),
     );
   }
 }
