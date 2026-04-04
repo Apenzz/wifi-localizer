7344d232bc1b5b66c58215991d646e4d735ec1ec
albertoDev <devalberto98@gmail.com>
2026-04-04 22:22:28 +0200

    add JSON serialization to Fingerprint model

--- lib/models/fingerprint.dart
+++ lib/models/fingerprint.dart
@@ -7,4 +7,20 @@ class Fingerprint {
   final DateTime timestamp;
 
   Fingerprint({required this.label, required this.networks, required this.timestamp});
+
+  Map<String, dynamic> toJson() {
+    return {
+      'label': label,
+      'networks': networks.map((n) => n.toJson()).toList(),
+      'timestamp': timestamp.toIso8601String(),
+    };
+  }
+
+  factory Fingerprint.fromJson(Map<String, dynamic> json) {
+    return Fingerprint(
+      label: json['label'] as String,
+      networks: (json['networks'] as List).map((n) => WifiNetwork.fromJson(n)).toList(),
+      timestamp: DateTime.parse(json['timestamp'] as String),
+    );
+  }
 }
